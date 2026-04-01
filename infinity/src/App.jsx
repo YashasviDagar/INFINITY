b1bc3b20c5154ee198af534f30d208f3976ca5eb
Yashasvi Dagar <dagaryashu2046@gmail.com>
2026-04-02 01:30:00 +0530

    Search Input Display

--- infinity/src/App.jsx
+++ infinity/src/App.jsx
@@ -11,11 +11,13 @@ import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import SearchBar from './components/SearchBar'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       <Navbar />{/**this navbar will be showed in all the pages as it is above the routers */}
+      <SearchBar />
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/collection' element={<Collection/>}/>
