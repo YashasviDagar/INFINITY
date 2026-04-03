4562b5976809a7bc38e16fbc6f71e9b638f65283
Yashasvi Dagar <dagaryashu2046@gmail.com>
2026-04-03 21:55:50 +0530

    Login and SignIn Page

--- infinity/src/pages/Login.jsx
+++ infinity/src/pages/Login.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [currentState,setCurrentState] = useState('Sigh Up')
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+  }
+
+
   return (
-    <div>
-      
-    </div>
+    <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-700'>
+      <div className='inline-flex items-center gap-2 mb-2 mt-10'>
+        <p className='prata-regular text-3xl'>{currentState}</p>
+        <hr className='border-none h-[1.5px] w-8 bg-gray-800' />
+      </div>
+      {/* we dont need name to login just mail */}
+      {currentState == 'Login' ? '' : <input type="text" className='w-full px-3 py-2 border border-gray-800' placeholder='Name' required/>}
+      <input type="email" className='w-full px-3 py-2 border border-gray-800' placeholder='Email' required/>
+      <input type="password" className='w-full px-3 py-2 border border-gray-800' placeholder='Password' required/>
+
+      <div className='w-full flex justify-between text-sm -mt-2'>
+        <p className='cursor-pointer'>Forgot Your Password?</p>
+        {
+          // this changes the UI between the Login and Sign Up state
+          currentState === 'Login'
+          ? <p onClick={()=>setCurrentState('Sign Up')} className='cursor-pointer'>Create Account</p>
+          : <p onClick={()=>setCurrentState('Login')} className='cursor-pointer'>Login Here</p>
+        }
+
+      </div>
+      <button className='bg-black text-white font-light px-8 py-2 mt-4'>{currentState === 'Login' ? 'Sign In' : 'Sign Up'}</button>
+    </form>
   )
 }
 
